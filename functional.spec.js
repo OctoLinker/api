4b28486101b809ad8e95a428113794f110f1cf05
Stefan Buck <stefanb@brandwatch.com>
2020-03-31 23:58:13 +0200

    Adjust API call after upgrading got

--- functional.spec.js
+++ functional.spec.js
@@ -20,12 +20,11 @@ describe('functional', () => {
 
   function testBulk(type, target, result) {
     it(`resolves ${target} from ${type} to ${result}`, async () => {
-      const response = await got.post({
-        json: true,
-        url: 'http://localhost:3000/',
-        body: [{ type, target }],
-      });
-      expect(response.body).toEqual({
+      const response = await got.post('http://localhost:3000/', {
+        json: [{ type, target }],
+      }).json();
+
+      expect(response).toEqual({
         result: [
           {
             result,
@@ -38,12 +37,8 @@ describe('functional', () => {
   }
 
   it('supports GET', async () => {
-    const response = await got.get({
-      json: true,
-      url:
-        'http://localhost:3000/?npm=jquery,request&go=k8s.io/kubernetes/pkg/api',
-    });
-    expect(response.body).toEqual({
+    const response = await got('http://localhost:3000/?npm=jquery,request&go=k8s.io/kubernetes/pkg/api').json();
+    expect(response).toEqual({
       result: [
         {
           result: 'https://github.com/jquery/jquery',
