b5f393c507c6a8532861dc776ff4f2e29a842629
yoyosource <yoyosource@nidido.de>
2021-09-28 15:11:12 +0200

    Add lombok and some other common dependencies fix mapping.json creation Update mapping.json

--- scripts/update-mapping-files.js
+++ scripts/update-mapping-files.js
@@ -8,14 +8,21 @@ const jsdom = require('jsdom'); // eslint-disable-line import/no-extraneous-depe
 const { JSDOM } = jsdom;
 
 const config = [
-  'http://junit.org/junit5/docs/current/api/allclasses-frame.html',
-  'http://static.javadoc.io/org.mockito/mockito-core/2.12.0/allclasses-frame.html',
+  'https://junit.org/junit5/docs/current/api/allclasses-index.html',
+  'https://www.javadoc.io/static/org.mockito/mockito-core/3.12.4/allclasses-frame.html',
+  'https://www.javadoc.io/static/org.mockito/mockito-core/2.12.0/allclasses-frame.html', // The original link 'http://static.javadoc.io/org.mockito/mockito-core/2.12.0/allclasses-frame.html' does not work correctly
   'https://www.slf4j.org/api/allclasses-frame.html',
   'http://hamcrest.org/JavaHamcrest/javadoc/2.0.0.0/allclasses-frame.html',
   'http://fasterxml.github.io/jackson-core/javadoc/2.9/allclasses-frame.html',
   'http://fasterxml.github.io/jackson-databind/javadoc/2.9/allclasses-frame.html',
   'http://fasterxml.github.io/jackson-annotations/javadoc/2.9/allclasses-frame.html',
   'http://hadoop.apache.org/docs/stable/api/allclasses-frame.html',
+  'http://projectlombok.org/api/allclasses-frame.html',
+  'http://www.atteo.org/static/classindex/apidocs/allclasses-frame.html',
+  'https://asm.ow2.io/javadoc/allclasses-index.html',
+  'http://hamcrest.org/JavaHamcrest/javadoc/2.2/allclasses-frame.html',
+  'http://logback.qos.ch/apidocs/allclasses-frame.html',
+  'http://yoyosource.github.io/YAPION/javadoc/v0.25.3/allclasses-index.html',
 ];
 
 async function loadPage(url) {
@@ -23,52 +30,82 @@ async function loadPage(url) {
     const { body } = await got(url);
     return new JSDOM(body).window.document;
   } catch (err) {
+    console.log("Could not fetch: " + url);
     return new JSDOM('<html></html>').window.document;
   }
 }
 
 async function getSpringDocumentationUrls() {
   const urlsToFetch = [];
-  const document = await loadPage('https://spring.io/docs/reference');
 
-  function findUrl(el, list) {
-    const node = el.parentNode.parentNode.querySelector('.api-link');
-    if (!node) return;
+  const document = await loadPage('https://spring.io/projects');
+  const linkNodes = document.querySelector('#filters-and-proj').querySelectorAll('a');
+  var allLinks = [];
+  linkNodes.forEach((el) => {
+    allLinks.push(el.href);
+  })
 
-    let url = node.href.replace('index.html', '');
-    url += 'allclasses-frame.html';
-
-    list.push(url);
-  }
-
-  document.querySelectorAll('.docs--item').forEach((elContainer) => {
-    const currentVersionEl = elContainer.querySelector('.icon-current-version');
-    const releaseVersionEl = elContainer.querySelector('.icon-ga-release');
-    const snapshotVersionEl = elContainer.querySelector(
-      '.icon-snapshot-release',
-    );
-
-    if (currentVersionEl) {
-      findUrl(currentVersionEl, urlsToFetch);
-    } else if (releaseVersionEl) {
-      findUrl(releaseVersionEl, urlsToFetch);
-    } else if (snapshotVersionEl) {
-      findUrl(snapshotVersionEl, urlsToFetch);
+  for (var link of allLinks) {
+    const currentDocument = await loadPage(link + "#learn");
+    if (!currentDocument) {
+      continue;
     }
-  });
-
-  return urlsToFetch;
+    const learnNode = currentDocument.querySelector('#learn');
+    if (!learnNode) {
+      continue;
+    }
+    const currentNode = learnNode.querySelectorAll('a');
+    var apiLinks = [];
+    currentNode.forEach((el) => {
+      const href = el.href;
+      if (href.includes('api')) {
+        apiLinks.push(href);
+      }
+    })
+    if (apiLinks.length == 0) {
+      continue;
+    }
+    apiLinks.forEach((el) => {
+      // Example link: https://docs.spring.io/spring-boot/docs/current/api/allclasses-frame.html
+      urlsToFetch.push(el.replace('index.html', '') + "/allclasses-frame.html");
+      urlsToFetch.push(el.replace('index.html', '') + "/allclasses-index.html");
+    })
+  }
+  return urlsToFetch.reverse();
 }
 
 async function getClassesUrl(results, url) {
   console.log(url);
   const document = await loadPage(url);
-  const baseUrl = url.replace('allclasses-frame.html', '');
+  const baseUrl = url.replace('allclasses-frame.html', '').replace('allclasses-index.html', '');
 
-  const nodes = document.querySelectorAll('a');
+  var nodes;
+  const node = document.querySelector('.allClassesContainer');
+  if (node) {
+    nodes = [];
+    var i = 0;
+    while (true) {
+      var current = node.querySelector('#i' + i);
+      if (!current) {
+        break;
+      }
+      nodes.push(current.querySelector('a'));
+      i++;
+    }
+  } else {
+    nodes = document.querySelectorAll('a');
+  }
+
+  if (url === 'https://junit.org/junit5/docs/current/api/allclasses-index.html') {
+    nodes = document.querySelector('div.summary-table.two-column-summary').querySelectorAll('a');
+  }
+
   if (nodes) {
     nodes.forEach((el) => {
       const link = el.href;
+      if (link.startsWith('http')) {
+        return;
+      }
       let library = link.replace(/\//g, '.');
 
       if (library.endsWith('.html')) {
